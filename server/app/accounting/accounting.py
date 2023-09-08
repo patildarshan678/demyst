d17320f6f841ded9f7b30d07bd239e08dc45a188
patildarshan678 <patildarshan678@gmail.com>
2023-09-08 13:23:41 +0530

    1.COmplete web app developement

--- server/app/accounting/accounting.py
+++ server/app/accounting/accounting.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 class Accounting:
     def __init__(self) -> None:
         pass
@@ -33,5 +34,32 @@ class Accounting:
             return sheet
         except BaseException as err:
             msg = f'Exception occured in fetch_balance_sheet.{err}'
+            print(msg)
+            return False
+        
+    def summarise_outcome(self,sheet,requested_loan):
+        try:
+            preAssesment = 20
+            current_date = datetime.today()
+            last_12_months_date = current_date - timedelta(days=30*12)
+            last_year = last_12_months_date.year
+            last_month = last_12_months_date.month
+            last_12_assest = 0
+            for row in sheet:
+                year,month,profitorloss,assetsValue = row.values()
+                if profitorloss < 0:
+                    loss = profitorloss
+                else:
+                    profit = profitorloss
+                if year <= last_year and month <=last_month and profit:
+                    preAssesment = 60
+                    last_12_assest = last_12_assest + assetsValue
+            asset_avg = last_12_assest / len(sheet)
+            if asset_avg >requested_loan:
+                preAssesment = 100
+            approved_loan = (preAssesment * requested_loan) / 100
+            return approved_loan, preAssesment
+        except BaseException as err:
+            msg = f" Exception occured in summarise_outcome.{err}"
             print(msg)
             return False
--- server/app/balances.py
+++ server/app/balances.py
@@ -10,8 +10,28 @@ def fetch_balance():
         account = Accounting()
         sheet = account.fetch_balance_sheet()
         if sheet:
-            compose_response(data=sheet)
+            return compose_response(data=sheet)
     except BaseException as err:
         msg = f'exception occured in fetch_baalace API. {err}'
+        print(msg)
+        return compose_response(data=msg,status='failed')
+    
+@balances_bp.route('/summit_sheet',methods=['POST'])
+def summit_sheet():
+    try:
+        request_data = request.get_json()
+        sheet = request_data.get('sheet')
+        loanamount = request_data.get('loanamount')
+        account = Accounting()
+        approved_loan,preAssesment = account.summarise_outcome(sheet=sheet,requested_loan=int(loanamount))
+        response = {
+            'ApprovedLoan' : approved_loan,
+            "preAssesment" : preAssesment
+            
+        }
+        if approved_loan:
+            return compose_response(data=response)
+    except BaseException as err:
+        msg = f'exception occured in summit_sheet API. {err}'
         print(msg)
         return compose_response(data=msg,status='failed')
